e0614291f9cc0252c7cb456df87ef7f9653f62f8
daniyal007-ui <daniyalkumroh007@gmail.com>
2025-01-22 11:39:44 +0500

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -67,7 +67,7 @@ export default function Hero() {
 
                 
 
-                <Link href={'https://api.whatsapp.com/message/GUTC3CMZFWAAK1?autoload=1&app_absent=0'} className={buttonVariants({ variant: "outline" })}>Customer Support</Link>
+                <Link href={'https://whatsapp.com/message/GUTC3CMZFWAAK1?autoload=1&app_absent=0'} className={buttonVariants({ variant: "outline" })}>Customer Support</Link>
 
             </div>
 
